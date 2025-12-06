b0e2afb768280d6541f1b265d4c63c794fb079f3
euriezz <salomaeurie@gmail.com>
2025-12-06 10:58:09 +0800

    Update num13.java

--- num13.java
+++ num13.java
@@ -5,14 +5,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class num13{
-
-public class PassFailCounter {
+public class num13 {
     public static void main(String[] args) {
         int[] grades = {80, 60, 90, 70, 85, 50};
         int passed = 0;
         int failed = 0;
-        
+
         for (int grade : grades) {
             if (grade >= 75) {
                 passed++;
@@ -20,16 +18,8 @@ public class PassFailCounter {
                 failed++;
             }
         }
-        
+
         System.out.println("Passed: " + passed + ", Failed: " + failed);
     }
 }
 
-
-
-
-
-
-
-
-}
