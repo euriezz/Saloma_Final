c6377a0abfa90e512f5d357a598c250ab849b521
euriezz <salomaeurie@gmail.com>
2025-12-06 10:39:43 +0800

    Update num7.java

--- num7.java
+++ num7.java
@@ -7,7 +7,7 @@
  */
 public class num7
 {
-    public static void convertNegativesToPositives() {
+    public static void main(String [] args) {
         int[] array = {-1, 2, -3, 4, -5};
         
         for (int i = 0; i < array.length; i++) {
